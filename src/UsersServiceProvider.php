2309cece245d6dde2c871ec07967b597c00b7d84
Nic Loomans <nic@bocapa.com>
2015-02-09 15:04:37 +1300

    Fixed routes file path

--- src/UsersServiceProvider.php
+++ src/UsersServiceProvider.php
@@ -52,7 +52,7 @@ class UsersServiceProvider extends ServiceProvider {
 
     public function boot()
     {
-        include __DIR__.'/../../routes.php';
+        include __DIR__.'/routes.php';
     }
 
 	/**
